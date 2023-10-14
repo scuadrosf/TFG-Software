380a0f3b410e85b1473f3589f355d6c2cbb2e114
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2023-10-14 20:00:28 +0200

    continue sidebar

--- allCode/Frontend/src/app/components/common-components/sidebar/sidebar.component.ts
+++ allCode/Frontend/src/app/components/common-components/sidebar/sidebar.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 import { SidebarService } from 'src/app/services/side-bar.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  // styles: ['.sidebar{display:block;}']
-
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
   isSidebarOpen: boolean = true;
 
-  constructor(private sidebarService: SidebarService) { }
+  constructor(private sidebarService: SidebarService, public authService: AuthService) { }
 
   ngOnInit(): void {
-    this.sidebarService.getSidebarState().subscribe(isOpen => {
-      this.isSidebarOpen = isOpen;
-    });
+    // this.sidebarService.getSidebarState().subscribe(isOpen => {
+    //   this.isSidebarOpen = isOpen;
+    // });
   }
 }
