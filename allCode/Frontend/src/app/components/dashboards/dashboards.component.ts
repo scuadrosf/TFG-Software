328c95c20e1886208cc13d1747c05b3eae9d4be6
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2023-12-11 17:28:41 +0100

    Header dashboard

--- allCode/Frontend/src/app/components/dashboards/dashboards.component.ts
+++ allCode/Frontend/src/app/components/dashboards/dashboards.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-dashboards',
-  templateUrl: './dashboards.component.html',
-  // styleUrls: ['./dashboards.component.scss']
+  templateUrl: './dashboards.component.html'
 })
-export class DashboardsComponent {
+export class DashboardsComponent implements OnInit {
+  isAdmin: boolean = false
+  currentUser!: User
+
+  constructor(public authService: AuthService) {
+    this.currentUser = this.authService.currentUser()
+
+
+  }
+
+  ngOnInit(): void {
+    this.isAdmin = this.authService.isAdmin();
+    // if (this.currentUser.roles.indexOf('ADMIN')) {
+    //   this.isAdmin = true
+    // } else {
+    //   this.isAdmin = false;
+    // }
+    console.log(this.isAdmin)
+  }
+
 
 }
--- allCode/Frontend/src/app/components/login/login.component.ts
+++ allCode/Frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -7,14 +8,15 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  isLogged: boolean = false;
 
-
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(public authService: AuthService, private router: Router) { }
 
   onLogin(user: string, pass: string) {
     this.authService.logIn(user, pass).subscribe(
       (_) => {
         this.router.navigate(['/dashboard'])
+        this.isLogged = this.authService.isLogged()
         console.log(this.authService.isLoggedIn)
         // window.location.reload();
       },
